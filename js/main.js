d700da9cb9d8a9fa671b2bd62315f41032f2f29e
Nash Vail <nishantve1@gmail.com>
2017-01-30 21:54:50 +0530

    Added comments to vector file

--- js/main.js
+++ js/main.js
@@ -8,43 +8,51 @@ window.onload = function () {
   const π = Math.PI;
 
   // Forces
-  let angle = π/4;
-  let centerPv = Vector.cartCreate(width / 2, height / 2);
-  let f1 = Vector.create(5, π/2);
-  let f2 = Vector.create(8, π);
+  let g = Vector.create(0, π/2);
 
-  let p1 = Particle.create(Vector.cartCreate(width / 2, height / 2 - 200), [f1, f2]);
-  // p1.velocity = Vector.create(10, 0);
+  let p1 = Particle.create(Vector.cartCreate(width / 2, height / 2 + 200), [g]);
+  let p2 = Particle.create(Vector.cartCreate(width / 2, height / 2), [g]);
+  p1.velocity = Vector.create(15, 0);
+  p2.mass = 2000;
 
-  let v = Vector.create();
+  let f1 = Vector.create(5, 0);
+  let f2 = Vector.create(5, 0);
+
+  p1.forces.push(f1);
+  p2.forces.push(f2);
+
+  let particles = [p1, p2];
 
   update();
 
   function update() {
     context.clearRect(0, 0, width, height);
-    p1.update();
 
-    if (p1.position.x + p1.radius > width) {
-      p1.position.setX(width - p1.radius);
-      p1.velocity.reverseX();
-    }
-    if (p1.position.x - p1.radius < 0) {
-      p1.position.setX(p1.radius);
-      p1.velocity.reverseX();
-    }
-    if (p1.position.y + p1.radius > height) {
-      p1.position.setY(height - p1.radius);
-      p1.velocity.reverseY();
-    }
-    if (p1.position.y - p1.radius < 0) {
-      p1.position.setY(p1.radius);
-      p1.velocity.reverseY();
+    for (var i = 0; i < particles.length; i++) {
+      let p = particles[i];
+      p.update();
+      if (p.position.x + p.radius > width) {
+        p.position.setX(width - p.radius);
+        p.velocity.reverseX();
+      }
+      if (p.position.x - p.radius < 0) {
+        p.position.setX(p.radius);
+        p.velocity.reverseX();
+      }
+      if (p.position.y + p.radius > height) {
+        p.position.setY(height - p.radius);
+        p.velocity.reverseY();
+      }
+      if (p.position.y - p.radius < 0) {
+        p.position.setY(p.radius);
+        p.velocity.reverseY();
+      }
+
+      p.draw(context);
     }
 
-    v = centerPv.subtractFrom(p1.position);
-    f1.setDirection(v.direction);
-    p1.draw(context);
-    
+    f1.setDirection(p2.position.subtractFrom(p1.position).direction);
+    f2.setDirection(p1.position.subtractFrom(p2.position).direction);
     requestAnimationFrame(update);
   }
 
--- js/vector.js
+++ js/vector.js
@@ -1,23 +1,54 @@
 let Vector = {
-  // Create with polar coordinates
+  /*
+  * Function: create
+  * -----------------
+  * Create a new Vector with given polar parameters.
+  */
   create(magnitude = 0, direction = 0) {
     return this.extend({
       magnitude: Math.abs(magnitude),
       direction
     });
   },
-  // Create with Cartesian coordinates
+
+  /*
+  * Function: cartCreate
+  * --------------------
+  * Create a new Vector with given Cartesian parameters.
+  */
   cartCreate(x, y) {
     let magnitude = Math.sqrt(x * x + y * y);
     let direction = Math.atan2(y, x);
     return this.create(magnitude, direction);
   },
+
+  /*
+  * Function: get x
+  * --------------------
+  * Returns the Cartesian X of the vector. 
+  * Relative to origin at top left.
+  */
   get x() {
     return Math.cos(this.direction) * this.magnitude;
   },
+
+  /*
+  * Function: get y
+  * --------------------
+  * Returns the Cartesian Y of the vector. 
+  * Relative to origin at top left.
+  */
   get y() {
     return Math.sin(this.direction) * this.magnitude;
   },
+
+  /*
+  * Function: quadrant
+  * -------------------------
+  * Returns the quadrant in which 
+  * the vector lies in. 
+  * Quadrant starts from 1 bottom right and clockwise onwards.
+  */
   get quadrant() {
     let x = this.x, y = this.y;
     if (x >= 0 && y >= 0) {
@@ -32,76 +63,162 @@ let Vector = {
       return -1;
     }
   },
+
+  /*
+  * Function: setX
+  * -------------------------
+  * Updates X end of the vector.
+  */
   setX(newX) {
     let newV = Vector.cartCreate(newX, this.y);
     this.setMagnitude(newV.magnitude);
     this.setDirection(newV.direction);
   },
+
+  /*
+  * Function: setY
+  * -------------------------
+  * Updates Y end of the vector.
+  */
   setY(newY) {
     let newV = Vector.cartCreate(this.x, newY);
     this.setMagnitude(newV.magnitude);
     this.setDirection(newV.direction);
   },
+
+  /*
+  * Function: setDirection
+  * -------------------------
+  * Updates the direction or the
+  * angle Vector makes with the positive 
+  * x axis.
+  */
   setDirection(angle) {
     this.direction = angle;
   },
+
+  /*
+  * Function: setMagnitude
+  * -------------------------
+  * Updates the magnitude of the vector.
+  */
   setMagnitude(mag) {
     this.magnitude = Math.abs(mag);
   },
+
   // Arithmetic
+
+  /*
+  * Function: addTo
+  * -------------------------
+  * Takes a second vector as parameter. Returns 
+  * a new Vector that is vector sum of this vector
+  * and the one passed as parameter.
+  */
   addTo(v) {
     let mag = Math.sqrt(Math.pow(this.x + v.x, 2) + Math.pow(this.y + v.y, 2));
     let dir = Math.atan2(this.y + v.y, this.x + v.x);
     return Vector.create(mag, dir);
   },
+
+  /*
+  * Function: subtractFrom
+  * -------------------------
+  * Takes a second vector as parameter. Returns 
+  * a new Vector that is vector difference of this vector
+  * and the one passed as parameter.
+  */
   subtractFrom(v) {
-    // Probably just use the reverse function below
     let rv = Vector.create(v.magnitude, v.direction + Math.PI);
     let mag = Math.sqrt(Math.pow(this.x + rv.x, 2) + Math.pow(this.y + rv.y, 2));
     let dir = Math.atan2(this.y + rv.y, this.x + rv.x);
     return Vector.create(mag, dir);
   },
+
+  /*
+  * Function: divideBy
+  * -------------------------
+  * Divides the magnitude of this vector 
+  * by the parameter. No new Vector is returned.
+  */
   divideBy(scalar) {
     return Vector.create(this.magnitude / scalar, this.direction);
   },
+
+  /*
+  * Function: angleBetween
+  * -------------------------
+  * Returns the angle (in radians) 
+  * between this vector and the vector
+  * passed in as parameter.
+  */
   angleBetween(v) {
     let num = this.x * v.x + this.y * v.y;
     let den = this.magnitude * v.magnitude;
     return Math.acos(num / den);
   },
-  // Reverses the direction of vector
+
+  /*
+  * Function: reverse
+  * -------------------------
+  * Reverses the direction of this vector.
+  */
   reverse() {
     console.log(this.magnitude);
     this.setDirection(this.direction + Math.PI);
   },
-  reverseX(damping = 1) {
+
+  /*
+  * Function: reverseX
+  * -------------------------
+  * Reverses direction only of the 
+  * X(horizontal) component of the vector.
+  */
+  reverseX() {
     let compX = this.cx(),
       compY = this.cy();
-
     compX.reverse();
     this.setDirection(compX.addTo(compY).direction);
-
-    let newMagnitude = Math.abs(this.magnitude * damping) < 0.5 ? 0 : Math.abs(this.magnitude * damping);
-    this.setMagnitude(newMagnitude);
   },
-  reverseY(damping = 1) {
+
+  /*
+  * Function: reverseY
+  * -------------------------
+  * Reverses direction only of the 
+  * Y(vertical) component of the vector.
+  */
+  reverseY() {
     let compX = this.cx(),
       compY = this.cy();
-
     compY.reverse();
     this.setDirection(compX.addTo(compY).direction);
-
-    let newMagnitude = Math.abs(this.magnitude * damping) < 0.5 ? 0 : Math.abs(this.magnitude * damping);
-    this.setMagnitude(newMagnitude);
   },
-  // X and Y component vectors of the main vector
+
+  /*
+  * Function: cx
+  * -------------------------
+  * Returns a new vector that represents
+  * the horizontal component of this vector.
+  */
   cx() {
     return Vector.create(this.magnitude * Math.cos(this.direction), this.quadrant === 1 || this.quadrant === 4 ? 0 : Math.PI);
   },
+
+ /*
+  * Function: cy
+  * -------------------------
+  * Returns a new vector that represents
+  * the vertical component of this vector.
+  */
   cy() {
     return Vector.create(this.magnitude * Math.sin(this.direction), this.quadrant === 1 || this.quadrant === 2 ? Math.PI / 2 : -Math.PI / 2);
   },
-  // Drawing the vector diagram
+
+  /*
+  * Function: draw
+  * --------------------
+  * Draws the vector on canvas.
+  */
   draw(context, sx = 0, sy = 0, color = '#000') {
     context.strokeStyle = color;
     context.beginPath();
